35a0caa622a06692f7439241199d58a4eae7983f
batuta <saelbatita@gmail.com>
2023-05-27 07:44:49 +0100

    add model product

---
+++ lib/models/product_model.dart
@@ -0,0 +1,10 @@
+
+class ProductModel{
+  final String title;
+  final double price;
+  final String category;
+  final String description;
+  final String image;
+
+  ProductModel(this.title, this.price, this.category, this.description, this.image);
+}
