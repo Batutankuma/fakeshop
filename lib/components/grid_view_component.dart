e86579b38a83041bd88cc57c54c8275830598283
Batuta Sael <saelbatita@gmail.com>
2023-09-20 00:06:21 +0200

    refactory

--- lib/components/grid_view_component.dart
+++ lib/components/grid_view_component.dart
@@ -1,4 +1,3 @@
-import 'package:fakeshop/constant.dart';
 import 'package:fakeshop/models/product_model.dart';
 import 'package:fakeshop/views/description_views.dart';
 import 'package:flutter/material.dart';
--- lib/views/login_views.dart
+++ lib/views/login_views.dart
@@ -1,8 +1,9 @@
+import 'package:fakeshop/constant.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class LoginView extends StatelessWidget {
-  TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
   final _keyForm = GlobalKey<FormState>();
 
@@ -11,32 +12,113 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Form(
         key: _keyForm,
         child: Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
-          child: Column(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 120),
+          child: ListView(
             children: [
-              const Text("Login Application"),
-              TextFormField(
-                controller: username,
-                decoration: const InputDecoration(
-                    hintText: "Username", labelText: "Username"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: password,
-                decoration: const InputDecoration(
-                    hintText: "Password", labelText: "Password"),
-              ),
-              const SizedBox(height: 10),
-              const FilledButton(
-                onPressed: null,
-                child: Center(
-                  child: Text("Login"),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Bonjour à Nouveau!".toUpperCase(),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.black,
+                      fontSize: 25),
                 ),
-              )
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Content de te revoir".toUpperCase(),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey,
+                      fontSize: 15),
+                ),
+              ),
+              const SizedBox(height: 40),
+              SizedBox(
+                height: 40,
+                child: TextFormField(
+                  controller: email,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      icon: Icon(Icons.email),
+                      hintText: "Email",
+                      labelText: "Email"),
+                ),
+              ),
+              const SizedBox(height: 40),
+              SizedBox(
+                height: 40,
+                child: TextFormField(
+                  controller: password,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      icon: Icon(Icons.key),
+                      hintText: "Password",
+                      labelText: "Password"),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topRight,
+                child: Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 14),
+                ),
+              ),
+              const SizedBox(height: 40),
+              //btn login
+              FilledButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                        Theme.of(context).primaryColor)),
+                onPressed: () => {
+                  //logic authentification application
+                  fireAuth
+                      .signInWithEmailAndPassword(
+                          email: email.text, password: password.text)
+                      .then((value) => print(value))
+                      .catchError((error) => print(error))
+                },
+                child: const Center(
+                  child: Text(
+                    "Login to your account",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              //btn login
+              ElevatedButton.icon(
+                label: const Center(
+                  child: Text(
+                    "Login Using Google",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                icon: const Icon(Icons.apple),
+                style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.red)),
+                onPressed: null,
+              ),
+              const SizedBox(height: 40),
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Besoin d'avoir un compte Sign Up?",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.black,
+                      fontSize: 14),
+                ),
+              ),
             ],
           ),
         ),
